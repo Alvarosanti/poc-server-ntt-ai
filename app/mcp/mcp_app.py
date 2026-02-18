8cdbda35c3a664f3d09e7b19e318837513a11dc1
4lv4r0 <alvarosantisteban56@gmail.com>
2026-02-18 09:13:07 -0500

    update app widget

--- app/mcp/mcp_app.py
+++ app/mcp/mcp_app.py
@@ -1,9 +1,11 @@
 from fastmcp import FastMCP
-from pathlib import Path
+from fastmcp.apps import App
 
-# Inicializa tu MCP server
 mcp = FastMCP("poc-server-ai")
 
-widgets_path = Path(__file__).parent / "widgets"
+my_app = App(
+    name="my-app",
+    resources_dir="app/mcp/apps/my-app"
+)
 
-mcp.mount_static("/widget", widgets_path)
+mcp.register_app(my_app)
--- app/mcp/tools/math_tools.py
+++ app/mcp/tools/math_tools.py
@@ -1,25 +1,25 @@
 from app.mcp.mcp_app import mcp
 from app.services.math_service import MathService
 
+
 @mcp.tool()
 def suma(a: float, b: float) -> float:
     return MathService.suma(a, b)
 
+
 @mcp.tool()
 def resta(a: float, b: float) -> float:
     return MathService.resta(a, b)
+
 
 @mcp.tool(
     description=(
         "Abre una interfaz visual para que el usuario ingrese dos números "
         "cuando quiera realizar una suma."
     ),
-    meta={
-        "openai/outputTemplate": "ui://widget/input-text.html",
-        "openai/toolInvocation/invoking": "Abriendo calculadora...",
-        "openai/toolInvocation/invoked": "Calculadora lista"
+    app={
+        "resourceUri": "ui://my-app/view.html"
     }
 )
 def sumar_ui() -> str:
-    return "Calculadora visual abierta."
-
+    return "Abriendo calculadora..."
